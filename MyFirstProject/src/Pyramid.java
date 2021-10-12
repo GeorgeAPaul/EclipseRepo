8911ccba7b0f98153de9e18986fbe6246ec559da
George Paul <george@pollardpaul.com>
2021-10-12 18:26:16 +0200

    commit

---
+++ MyFirstProject/src/Pyramid.java
@@ -0,0 +1,32 @@
+
+public class Pyramid {
+	
+	private double height;
+	private double baseLength;
+
+	public Pyramid(double height, double baseLength) {
+		this.height = height;
+		this.baseLength = baseLength;
+		
+	}
+	
+	public double getVolume() {
+		
+		return (baseLength * baseLength * height)/3;
+	}
+	public double getSurfaceArea() {
+		
+		return Math.pow(baseLength, 2) + 2 * baseLength* Math.sqrt(Math.pow(baseLength,2)/4+Math.pow(height,2));
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Pyramid p = new Pyramid(4,4);
+		
+		System.out.println(p.getVolume());
+		System.out.println(p.getSurfaceArea());
+
+	}
+
+}
