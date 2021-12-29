aa1297cc8410bd1d1858c550024e59378d49a8a8
George Paul <george@pollardpaul.com>
2021-12-29 23:04:22 +0000

    asdasdasd

--- PirateParty/src/Weapon.java
+++ PirateParty/src/Weapon.java
@@ -1,27 +1,50 @@
 /**
- * 
+ * Class to represent Weapons in the game
  * 
  * @author George Paul
  *
  */
 public class Weapon extends Item {
 
+	/**
+	 * Represents how much damage the weapon does.
+	 */
 	private int damage;
-	private String[] weaponList = {"Sword", "Gun", "Sausage", "Bow", "Whip", "Bazooka"};
-	private int[] weaponDamageList = {50, 90, 0, 75, 20, 500};
 	
+	/**
+	 * List of possible weapons in the game, used for assigning damage when weapon is created.
+	 */
+	private String[] weaponList = {"Sword", "Gun", "Sausage", "Bow", "Whip", "Bazooka"};
+	
+	/**
+	 * List of possible weapon damages in the game, used for assigning damage when weapon is created.
+	 */
+	private int[] weaponDamageList = {50, 90, 0, 75, 20, 500};
+	//Not ideal as this list of weapons and damage needs to be updated here and in map class.
+	
+	/**
+	 * Constructor method
+	 * 
+	 * @param name of weapon
+	 */
 	public Weapon(String name) {
 		super(name);
 		
-		for(int i = 0; i < weaponList.length; i++) {
+		for(int i = 0; i < weaponList.length; i++) {//Loop over weapon list until weapon found, assign damage
 			if(name == weaponList[i]) {
 				this.damage = weaponDamageList[i];
+				break;
 			}
 		}
 		
 		
 	}
 	
+	/**
+	 * Method to return damage dealt by weapon
+	 * 
+	 * @return damage attackPower of weapon
+	 */
 	public int getDamage() {
 		return damage;
 	}
